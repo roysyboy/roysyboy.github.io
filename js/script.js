8fa160f971430fd8cb3f11620f460956f9ce9cbc
Roy Park <roydepark97@gmail.com>
2024-04-05 22:29:05 -0700

    change editor-like style and add images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Your JavaScript code here. Example:
-    console.log("Document loaded");
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+        navbar.innerHTML = `
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About Me</a></li>
+                        <li><a href="projects.html">Projects</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+        `;
+    }
+
+    const colors = ['green', 'yellow', 'light-blue'];
+    const projects = document.querySelectorAll('.project h3');
+    console.log(projects)
+    
+    projects.forEach((project, index) => {
+        project.classList.add(colors[index % colors.length]);
+        let binaryNumber = (index + 1).toString(2);
+        project.textContent = binaryNumber + ": " + project.textContent;
+    });
 });
